abf65a580b9aa3ca4ed49df3168a77cd8075139d
timothytenzin20 <tkhan12@uoguelph.ca>
2025-08-06 14:07:29 -0400

    transmit data from raspberry pi

--- server/client.py
+++ server/client.py
@@ -1,6 +1,48 @@
-import websocket
-ws = websocket.WebSocket()
-ws.connect("ws://localhost:3000?token=1239165", subprotocols=["secure-guelph-user"])
-ws.send("Hello, Server")
-print(ws.recv())
-ws.close()
+# pip install python-dotenv websocket-client
+import os
+from dotenv import load_dotenv
+from websocket import create_connection
+
+# Close connection
+def closeConnection(ws):
+    ws.close()
+    print("WebSocket closed.")
+
+# Create WebSocket connection
+def startConnection():
+    # Load environment variables from .env
+    load_dotenv()
+
+    # Get environment variables
+    wsToken = os.getenv("REACT_APP_TOKEN")
+    wsProtocol = os.getenv("REACT_APP_PROTOCOL")
+
+    # Check if values are loaded
+    if not wsToken or not wsProtocol:
+        raise ValueError("Missing REACT_APP_TOKEN or REACT_APP_PROTOCOL in .env file")
+
+    # Use for Raspberry Pi deployment
+    # url = f"ws://10.16.4.168:3000?token={wsToken}"
+
+    # Use for local development
+    url = f"ws://localhost:3000?token={wsToken}"
+
+    ws = create_connection(url, subprotocols=[wsProtocol])
+    print("Connected to server.")
+    return ws
+
+# Send printer data to websocket
+def transmitData():
+    ws = startConnection()
+    try:
+        with open('transmitData.json', 'r') as file:
+            content = file.read()
+        print("Sending JSON data...")
+        ws.send(content)
+    except FileNotFoundError:
+        print("Error: data.json not found.")
+    finally:
+        closeConnection(ws)
+
+if __name__ == "__main__":
+    transmitData()
